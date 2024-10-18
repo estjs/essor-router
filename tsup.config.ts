423c085147cc40b971f508d0290450d161bd6413
jiangxd <jiangxd2016@gmail.com>
2024-10-18 16:57:22 +0800

    feat: update compile and dep

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,23 @@
 import process from 'node:process';
 import { defineConfig } from 'tsup';
+import pkg from './package.json';
 
 const env = process.env.NODE_ENV;
+
+
+const banner = `/**
+* ${pkg.name} v${pkg.version}
+* (c) 2023-Present jiangxd <jiangxd2016@gmail.com>
+* @license MIT
+**/`;
 
 export default defineConfig({
   entryPoints: {
     'essor-router': './src/index.ts',
   },
-
+  banner: {
+    js: banner,
+  },
   outDir: 'dist',
   format: ['cjs', 'esm'],
   target: 'es2016',
@@ -18,11 +28,11 @@ export default defineConfig({
   cjsInterop: true,
   minify: env === 'production' ? true : false,
   tsconfig: './tsconfig.build.json',
+  external: ['essor'],
   define: {
     __DEV__: env !== 'production' ? 'true' : 'false',
     __TEST__: 'false',
   },
-
   outExtension({ format }) {
     return {
       js: `${env !== 'production' ? '.dev' : ''}.${format === 'esm' ? 'm' : 'c'}js`,
